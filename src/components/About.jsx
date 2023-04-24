e73e2b972caf446cdba0530c31e39b149e1fb2e7
Benric08 <richy8.26.05@gmail.com>
2023-04-23 22:46:46 -0500

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,8 @@ export default class About extends Component {
     return (
       <div>
         <h1>Hola, Soy Richard</h1>
+        <h2>vamos por un trabajo nuevo como desarrollador
+        </h2>
       </div>
     )
   }
